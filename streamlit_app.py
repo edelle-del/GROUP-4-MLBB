a1033858494c775626b3cf36506136ac66166d10
edelle-del <155860200+edelle-del@users.noreply.github.com>
2024-11-05 12:21:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,24 @@
 import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+import networkx as nx
+import pandas as pd
+import seaborn as sns
+import altair as alt
+from wordcloud import WordCloud
+from google.colab import files
+from mpl_toolkits.mplot3d import Axes3D
+from pandas.plotting import scatter_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.cluster import KMeans
+from sklearn.semi_supervised import LabelPropagation
+from sklearn.impute import SimpleImputer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.utils import resample
 
 # Set page configuration
 st.set_page_config(
